1029629479217d4f242bf5c741a6501cb1a3623a
lucca-falbo <luccafalbo@gmail.com>
2021-09-03 15:26:46 -0300

    add 5.2 first exercises

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-JavaScript-Trabalhando-com-elementos/scripts.js
@@ -0,0 +1,41 @@
+// Exercise 1
+
+console.log("Exercise 1")
+console.log(document.querySelector('#elementoOndeVoceEsta'))
+
+// Exercise 2
+
+console.log("Exercise 2")
+console.log(document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = "red")
+
+// Exercise 3
+
+console.log("Exercise 3")
+console.log(document.querySelector('#primeiroFilhoDoFilho').innerText = "Um texto teste")
+
+// Exercise 4
+
+console.log("Exercise 4")
+console.log(document.querySelector('#pai').firstElementChild)
+
+// Exercise 5
+
+console.log("Exercise 5")
+console.log(document.querySelector('#elementoOndeVoceEsta').previousElementSibling)
+
+// Exercise 6
+
+console.log("Exercise 6")
+console.log(document.querySelector('#elementoOndeVoceEsta').parentNode.innerText)
+
+// Exercise 7
+
+console.log("Exercise 7")
+console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling)
+
+// Exercise 8
+
+console.log("Exercise 8")
+console.log(document.querySelector('#pai').lastElementChild.previousElementSibling)
+
+
