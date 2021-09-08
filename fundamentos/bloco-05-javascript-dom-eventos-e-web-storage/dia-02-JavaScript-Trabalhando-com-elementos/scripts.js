1b17355f07262ecd94799cabcc41363792e049cf
lucca-falbo <luccafalbo@gmail.com>
2021-09-08 12:15:48 -0300

    add 5.2 main exercises

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-JavaScript-Trabalhando-com-elementos/scripts.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-JavaScript-Trabalhando-com-elementos/scripts.js
@@ -38,4 +38,40 @@ console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling)
 console.log("Exercise 8")
 console.log(document.querySelector('#pai').lastElementChild.previousElementSibling)
 
+// Exercise 9
 
+function exercise9(){
+    let elementoPai = document.querySelector("#pai")
+    let novoIrmao = document.createElement('section')
+    novoIrmao.setAttribute('id', 'irmaoCriadoAgora')
+    novoIrmao.innerText = 'eu fui criado agora'
+    elementoPai.appendChild(novoIrmao)
+}
+exercise9()
+
+// Exercise 10
+
+function exercise10(){
+    let elementoPai = document.querySelector("#elementoOndeVoceEsta")
+    let novoFilho = document.createElement('section')
+    novoFilho.setAttribute('id', 'filhoCriadoAgora')
+    novoFilho.innerText = 'eu sou o novo filho'
+    elementoPai.appendChild(novoFilho)
+}
+exercise10()
+
+// Exercise 11
+
+function exercise11(){
+    let elementoPai = document.querySelector("#primeiroFilhoDoFilho")
+    let novoFilho = document.createElement('section')
+    novoFilho.setAttribute('id', 'filhoDofilhoCriadoAgora')
+    novoFilho.innerText = 'eu sou o novo filho do filho'
+    elementoPai.appendChild(novoFilho)
+}
+exercise11()
+
+// Exercise 12
+
+console.log("Exercise 12")
+console.log(document.querySelector('#filhoDofilhoCriadoAgora').parentNode.parentNode.nextElementSibling)
