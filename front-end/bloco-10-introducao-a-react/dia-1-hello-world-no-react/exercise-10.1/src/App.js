38dd6d0f8225ecfd49ed03efce33e37fdc9b1086
lucca-falbo <luccafalbo@gmail.com>
2021-10-27 15:55:55 -0300

    exercises 1 and 2

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise-10.1/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const Task = (value) => {
+  return <li>{value}</li>;
+};
+
+const appointments = ["dentista", "academia", "psicologo", "aula"];
+
+function App() {
+  return (
+    <div className="App">
+      {appointments.map((appointment) => Task(appointment))}
+    </div>
+  );
+}
+
+export default App;
